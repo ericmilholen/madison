583baaa1083ea734dc22cd9dc929b01b1d87b7a7
eric milholen <ericmilholen@gmail.com>
2021-08-06 16:46:19 -0400

    s

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+	name: 'Home',
+	components: {
+		HelloWorld
+	},
+
+	data() {
+		return {}
+	},
+
+	methods: {
+		getTenDaysAgoUnix() {
+			var tenDaysAgo = new Date()
+			tenDaysAgo.setDate(tenDaysAgo.getDate() - 10)
+
+			console.log('ten days ago:', tenDaysAgo.getTime());
+			return tenDaysAgo.getTime()
+		},
+
+		getNowUnix() {
+			console.log('now', Date.now());
+			return Date.now()
+		},
+
+		getStockData() {
+			const finnhub = require('finnhub')
+	
+			const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+			api_key.apiKey = "c46p04aad3iagvmhcglg"
+	
+			const finnhubClient = new finnhub.DefaultApi()
+	
+			finnhubClient.stockCandles(
+				"AAPL",
+				"D",
+				1590988249, //this.getTenDaysAgoUnix(),
+				1591852249, //this.getNowUnix(),
+				{},
+				(error, data, response) => {
+					console.log(data),
+	
+					console.log(response);
+				}
+			)
+		},
+	},
+
+	created() {
+		this.getStockData()
+	}
 }
 </script>
